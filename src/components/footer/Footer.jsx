f5e931903257cb743bd680d412aef8ca372ce163
Priyabrata Khan <93465026+priyabratakhan@users.noreply.github.com>
2022-06-18 00:50:19 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer_div'>
+        <div>
+          <span>Customer Service</span>
+          <span>Order Status</span>
+          <span>Guest Returns</span>
+          <span>Shipping & Return Policy</span>
+          <span>Gift Cards</span>
+          <span>FAQ</span>
+          <span>Contact Us</span>
+        </div>
+        <div>
+          <span>About Us</span>
+          <span>About Our Brand</span>
+          <span>The Nordy Club</span>
+          <span>Store Locator</span>
+          <span>All Brands</span>
+          <span>Careers</span>
+          <span>Get Email Updates</span>
+          <span>Sephora Blog</span>
+          <span>Nordy Podcast</span>
+        </div>
+        <div>
+          <span>Sephora Rack & The Community</span>
+          <span>Corporate Social Responsibility</span>
+          <span>Diversity, Inclusion & Belonging</span>
+          <span>Big Brothers Big Sisters</span>
+          <span>Donate Clothes</span>
+        </div>
+        <div>
+          <span>Sephora Card</span>
+          <span>Apply for a Sephora card</span>
+          <span>Pay My Bill</span>
+          <span>Manage my Sephora Card</span>
+        </div>
+        <div>
+          <span>Sephora, Inc.</span>
+          <span>Sephora</span>
+          <span>Sephora Canada</span>
+          <span>Trunk Club</span>
+          <span>HauteLook</span>
+          <span>Investor Relations</span>
+          <span>Press Releases</span>
+          <span>Sephora Media Network</span>
+        </div>
+        <div>
+          <div>Dowload Our Apps</div>
+          <div>
+            <i class='uil uil-facebook-f'></i>
+            <i class='uil uil-twitter'></i>
+            <i class='uil uil-instagram'></i>
+            <i class='uil uil-linkedin-alt'></i>
+          </div>
+        </div>
+      </div>
+      <span>
+        <span>Your Privacy Rights</span>
+        <span>Do Not Sell My Info</span>
+        <span>Terms & Conditions</span>
+        <span>Internet Based Ads</span>
+        <span>California Supply Chain Act</span>
+        <span>2022 Sephora Rack</span>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
